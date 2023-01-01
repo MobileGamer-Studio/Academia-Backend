e4eabd7a0daa012f113bcf78764cb71dce48c415
MobileGamer-hq <73354088+MobileGamer-hq@users.noreply.github.com>
2023-01-01 22:50:34 +0100

    Render test 2

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
 
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+app.get("/", (req, res) => res.type('html').send(html));
 
-
-const serviceAccount = require("./academia-c3d0e-firebase-adminsdk-o1rr9-7e2532c12c.json");
-
-initializeApp({
-  credential: cert(serviceAccount),
-  databaseURL: "https://academia-c3d0e-default-rtdb.firebaseio.com"
-});
-
-const firestore = getFirestore();
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
