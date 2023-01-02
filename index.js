fee6c6eb1b7815ac1de57133525b6ea48efa23c1
MobileGamer-hq <73354088+MobileGamer-hq@users.noreply.github.com>
2023-01-02 13:51:20 +0100

    new fixes to server getting and setting user data

--- index.js
+++ index.js
@@ -30,8 +30,77 @@ app.listen(port, async () => {
 
         fs.writeFileSync("./data/products.json", JSON.stringify(products), 'utf8')
 
-        products.forEach(product => {
-            console.log(product.id + " added to products.json");
+
+        firestore.collection('Users').get().then(snapshot => {
+            const users = snapshot.docs.map(doc => doc.data());
+
+            const suggestedProducts = products.sort((p1, p2) => (p1.ratings < p2.ratings) ? 1 : (p1.ratings > p2.ratings) ? -1 : 0);
+            const suggestedUsers = users.sort((p1, p2) => (p1.userInfo.rating < p2.userInfo.rating) ? 1 : (p1.userInfo.rating > p2.userInfo.rating) ? -1 : 0);
+            suggestedUsers.sort((p1, p2) => (p1.sellerInfo.rating < p2.sellerInfo.rating) ? 1 : (p1.sellerInfo.rating > p2.sellerInfo.rating) ? -1 : 0);
+            const bestSellers = products.sort((p1, p2) => (p1.sold < p2.sold) ? 1 : (p1.sold > p2.sold) ? -1 : 0);
+            const activity = []
+
+            users.forEach(user => {
+                user.userInfo.recentlyViewed.forEach(productId => {
+                    products.forEach(product => {
+                        if (product.id == productId) {
+                            activity.push(product);
+                        }
+                    })
+                })
+            });
+
+            const sales = products.sort((p1, p2) => (p1.discount < p2.discount) ? 1 : (p1.discount > p2.discount) ? -1 : 0);
+
+            let spList = [];
+            suggestedProducts.forEach(product => {
+                spList.push(product.id);
+            })
+
+            let suList = [];
+            suggestedUsers.forEach(user => {
+                suList.push(user.id);
+            })
+
+            let bsList = [];
+            bestSellers.forEach(product => {
+                bsList.push(product.id);
+            })
+
+            let aList = [];
+            activity.forEach(product => {
+                aList.push(product.id);
+            })
+
+            let sList = [];
+            sales.forEach(product => {
+                sList.push(product.id);
+            })
+
+
+
+            const defaultFeed = {
+                suggestedProducts: spList,
+                suggestedUsers: suList,
+                bestSellers: bsList,
+                activity: aList,
+                newProducts: [],
+                sales: sList,
+            }
+
+
+
+            fs.writeFileSync("./data/feed.json", JSON.stringify(defaultFeed), 'utf8')
+
+            users.forEach(user => {
+                user.userInfo.feed = defaultFeed;
+                firestore.collection('Users').doc(user.id).set(user);
+                console.log(user.name + " feed updated");
+            })
+
+            console.log(defaultFeed);
+
+
         })
     }, err => {
         console.log(`Encountered error: ${err}`);
@@ -42,79 +111,9 @@ app.listen(port, async () => {
         const users = snapshot.docs.map(doc => doc.data());
 
         fs.writeFileSync("./data/users.json", JSON.stringify(users), 'utf8')
-
-
     }, err => {
         console.log(`Encountered error: ${err}`);
     })
-
-    const users = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'));
-    const products = JSON.parse(fs.readFileSync('./data/products.json', 'utf8'));
-
-    const suggestedProducts = products.sort((p1, p2) => (p1.ratings < p2.ratings) ? 1 : (p1.ratings > p2.ratings) ? -1 : 0);
-    const suggestedUsers = users.sort((p1, p2) => (p1.userInfo.rating < p2.userInfo.rating) ? 1 : (p1.userInfo.rating > p2.userInfo.rating) ? -1 : 0);
-    suggestedUsers.sort((p1, p2) => (p1.sellerInfo.rating < p2.sellerInfo.rating) ? 1 : (p1.sellerInfo.rating > p2.sellerInfo.rating) ? -1 : 0);
-    const bestSellers = products.sort((p1, p2) => (p1.sold < p2.sold) ? 1 : (p1.sold > p2.sold) ? -1 : 0);
-    const activity = []
-    users.forEach(user => {
-        user.userInfo.recentlyViewed.forEach(productId => {
-            products.forEach(product => {
-                if (product.id == productId) {
-                    activity.push(product);
-                }
-            })
-        })
-    });
-    const sales = products.sort((p1, p2) => (p1.discount < p2.discount) ? 1 : (p1.discount > p2.discount) ? -1 : 0);
-
-    let spList = [];
-    suggestedProducts.forEach(product => {
-        spList.push(product.id);
-    })
-
-    let suList = [];
-    suggestedUsers.forEach(user => {
-        suList.push(user.id);
-    })
-
-    let bsList = [];
-    bestSellers.forEach(product => {
-        bsList.push(product.id);
-    })
-
-    let aList = [];
-    activity.forEach(product => {
-        aList.push(product.id);
-    })
-
-    let sList = [];
-    sales.forEach(product => {
-        sList.push(product.id);
-    })
-
-
-
-    const defaultFeed = {
-        suggestedProducts: spList,
-        suggestedUsers: suList,
-        bestSellers: bsList,
-        activity: aList,
-        newProducts: [],
-        sales: sList,
-    }
-
-
-
-    fs.writeFileSync("./data/feed.json", JSON.stringify(defaultFeed), 'utf8')
-
-    users.forEach(user => {
-        user.userInfo.feed = defaultFeed;
-        firestore.collection('Users').doc(user.id).set(user);
-        console.log(user.name + " feed updated");
-    })
-
-
-    console.log(defaultFeed);
 });
 
 
